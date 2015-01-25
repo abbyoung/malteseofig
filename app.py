66dbfe2fc0bbbd04e0e08567dd249bc26d25eb02
Abigail Young <Abigail@sherry.local>
2015-01-24 22:14:28 -0800

    setting up base and index templates

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, session, url_for, flash
 
+
 app = Flask(__name__)
+
+# Adding PyJade because Jade is wonderful
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route("/")
 def index():
